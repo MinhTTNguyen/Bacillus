0ec3e24f3139e53e65852651e6ddefd1712c1735
MinhTTNguyen <32077384+MinhTTNguyen@users.noreply.github.com>
2018-05-11 14:09:40 -0400

    initial

---
+++ download_IMG.pl
@@ -0,0 +1,41 @@
+#! /usr/bin/perl -w
+# July 11th 2017
+# Download CAZy tables
+# Input: list of CAZy families
+# Output: folders containing html files and summarized table
+
+use strict;
+#use File::Download;
+
+my $path="/home/mnguyen/Research/Bacillus";
+my $filein_IMGID_list="Small_size.txt";
+my $folderout="IMG_small_size_30Apr2018";
+mkdir "$path/$folderout";
+
+#========================================================================================================#
+# Get list of CAZy families
+my @IMGID_list;
+open(List,"<$filein_IMGID_list") || die "Cannot open file $filein_IMGID_list";
+while (<List>)
+{
+	$_=~s/\s*//g;
+	push(@IMGID_list,$_);
+}
+close(List);
+#========================================================================================================#
+
+
+#========================================================================================================#
+my $cmd='curl https://signon-old.jgi.doe.gov/signon/create --data-urlencode login=thitrucminh.nguyen@concordia.ca --data-urlencode password=MinhTri1 -c ./cookies > /dev/null';
+print "$cmd\n";
+`$cmd`;
+foreach my $IMG_ID (@IMGID_list)
+{
+	my $url='https://genome.jgi.doe.gov/portal/IMG_'.$IMG_ID.'/download/download_bundle.tar.gz';#https://genome.jgi.doe.gov/portal/IMG_2643221487/download/download_bundle.tar.gz
+	#print $url. "\n";
+	my $cmd = "curl \"$url\" -o $path/$folderout/$IMG_ID.tar.gz -b ./cookies";
+	print "$cmd\n";
+	`$cmd`;
+	
+}
+#========================================================================================================#
